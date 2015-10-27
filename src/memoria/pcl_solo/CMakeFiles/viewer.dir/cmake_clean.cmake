c8890fec65181071f6f03258f99863ef10dcb200
Maximiliano Castro <macguionbajo@gmail.com>
2015-10-26 22:39:28 -0300

    Se añaden tests de pcl independiente de ROS

---
+++ src/memoria/pcl_solo/CMakeFiles/viewer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/viewer.dir/src/viewer.cpp.o"
+  "viewer.pdb"
+  "viewer"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/viewer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ src/memoria/pcl_solo/src/viewer.cpp
@@ -0,0 +1,37 @@
+#include <boost/thread/thread.hpp>
+#include <pcl/common/common_headers.h>
+#include <pcl/io/io.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/visualization/pcl_visualizer.h>
+
+boost::shared_ptr<pcl::visualization::PCLVisualizer> rgbVis (pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr cloud)
+{
+  // --------------------------------------------
+  // -----Open 3D viewer and add point cloud-----
+  // --------------------------------------------
+  boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer (new pcl::visualization::PCLVisualizer ("3D Viewer"));
+  viewer->setBackgroundColor (0.7,0.7,0.7);
+  pcl::visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGB> rgb(cloud);
+  viewer->addPointCloud<pcl::PointXYZRGB> (cloud, rgb, "sample cloud");
+  viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 3, "sample cloud");
+  viewer->addCoordinateSystem (1.0);
+  viewer->initCameraParameters ();
+  return (viewer);
+}
+
+
+int main(int argc, char **argv){
+    if (argc < 2){
+        printf("Error: Debe especificar un archivo .pcd\n");
+        exit(1);
+    }
+    // Cargar nube de puntos
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZRGB>);
+    pcl::io::loadPCDFile(argv[1],*cloud);
+    // Crear visualizador
+    boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer = rgbVis(cloud);
+    while (!viewer->wasStopped()){
+        viewer->spinOnce(100);
+        boost::this_thread::sleep (boost::posix_time::microseconds (100000));
+    }
+}
