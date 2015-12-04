9c83261613f91e3d5370162df6e62c3b34c71685
Maximiliano Castro <macguionbajo@gmail.com>
2015-12-04 16:59:07 -0300

    Añadidas constantes para BaseDriver

--- src/memoria2/src/Util.h
+++ src/memoria2/src/Util.h
@@ -11,6 +11,7 @@ class Util{
         // CONSTANTES PARA TODO EL QUE IMPORTE ESTA CLASE
         // Frames
         static const string BASE_FRAME;
+        static const string ODOM_FRAME;
         static const string CAMERA_FRAME;
         static const string KINECT_FRAME;
 
@@ -18,9 +19,15 @@ class Util{
         static const string KINECT_TOPIC;
         static const string GRIPPER_GOAL_TOPIC_SUFFIX;
         static const string GRIPPER_STATUS_TOPIC_SUFFIX;
+        static const string BASE_CONTROLLER_TOPIC;
+        static const string ODOM_TOPIC;
 
         // Otros
         static const float PI;
+
+        // Métodos
+        static float toRad(float grad);
+        static float toGrad(float rad);
     protected:
     private:
 };
--- src/memoria2/src/Util/Util.cpp
+++ src/memoria2/src/Util/Util.cpp
@@ -1,7 +1,10 @@
 #include "../Util.h"
 
+// PUBLIC
+// CONSTANTES
 // Frames
 const string Util::BASE_FRAME                  = "/base_footprint";
+const string Util::ODOM_FRAME = "/odom_combined";
 const string Util::CAMERA_FRAME                = "/high_def_frame";
 const string Util::KINECT_FRAME                = "/head_mount_kinect_ir_optical_frame";
 
@@ -9,12 +12,17 @@ const string Util::KINECT_FRAME                = "/head_mount_kinect_ir_optical_
 const string Util::KINECT_TOPIC                = "head_mount_kinect/depth_registered/points";
 const string Util::GRIPPER_GOAL_TOPIC_SUFFIX   = "_gripper_controller/gripper_action/goal";
 const string Util::GRIPPER_STATUS_TOPIC_SUFFIX = "_gripper_controller/gripper_action/result";
+const string Util::BASE_CONTROLLER_TOPIC       = "/base_controller/command";
+const string Util::ODOM_TOPIC = "/base_odometry/odom";
 
 // Otros
 const float Util::PI  = 3.1416;
 
-
-Util::Util()
-{
-    //ctor
+// MÉTODOS
+Util::Util(){}
+float Util::toGrad(float rad){
+	return rad*180.0/PI;
+}
+float Util::toRad(float grad){
+	return grad*PI/180.0;
 }
