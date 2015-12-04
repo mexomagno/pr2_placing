de4841434d82309e7b3b8c09dbb2b85feac273bb
Maximiliano Castro <macguionbajo@gmail.com>
2015-12-04 17:14:35 -0300

    Agregada funcionalidad 'turn'

--- src/memoria2/src/RobotBaseDriver.h
+++ src/memoria2/src/RobotBaseDriver.h
@@ -1,13 +1,29 @@
 #ifndef ROBOTBASEDRIVER_H
 #define ROBOTBASEDRIVER_H
+#include "Util.h"
+#include <ros/ros.h>
+#include <nav_msgs/Odometry.h>
+#include <geometry_msgs/Twist.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <tf/transform_listener.h>
 
 
 class RobotBaseDriver
 {
+	ros::NodeHandle *nh_;
+
     public:
+    	// Constructor y Destructor
         RobotBaseDriver();
+        ~RobotBaseDriver();
+        // Métodos
+        //goToPose(const string frame_id, geometry_msgs::PoseStamped pose);
+        bool goToPose();
     protected:
     private:
+    	bool turn(float angle);
+    	bool travel(float distance, float angle);
+    	// void odomCallback(const nav_msgs::Odometry::ConstPtr& odom);
 };
 
 #endif // ROBOTBASEDRIVER_H
--- src/memoria2/src/RobotDriver/RobotBaseDriver.cpp
+++ src/memoria2/src/RobotDriver/RobotBaseDriver.cpp
@@ -1,5 +1,130 @@
-#include "RobotBaseDriver.h"
+#include "../RobotBaseDriver.h"
+// CONSTANTES
+const float WAIT_TF_TIMEOUT = 1.0;
+const float LOOP_FREQ = 10.0;
+const float TWIST_VELOCITY = 2.5;
+const float ANGULAR_VELOCITY = 3;
+bool store_odom = false;
+tf::Vector3 last_position(0,0,0);
+tf::Quaternion last_orientation(0,0,0,1);
+ros::Subscriber odom_sub_;
+ros::Publisher base_cmd_pub_;
 
-RobotBaseDriver::RobotBaseDriver(){
-    //ctor
+// PUBLIC
+// Métodos
+void odomCallback(const nav_msgs::Odometry::ConstPtr& odom){
+	if (not store_odom)
+        return;
+    ROS_DEBUG("RobotBaseDriver: Recibida odometría: (%f,%f,%f) (%f,%f,%f,%f)",odom->pose.pose.position.x,odom->pose.pose.position.y,odom->pose.pose.position.z,odom->pose.pose.orientation.x,odom->pose.pose.orientation.y,odom->pose.pose.orientation.z,odom->pose.pose.orientation.w);
+    // Actualizar última posición
+    last_position.setX(odom->pose.pose.position.x);
+    last_position.setY(odom->pose.pose.position.y);
+    last_position.setZ(odom->pose.pose.position.z);
+
+    // Actualizar última orientación
+    last_orientation.setX(odom->pose.pose.orientation.x);
+    last_orientation.setY(odom->pose.pose.orientation.y);
+    last_orientation.setZ(odom->pose.pose.orientation.z);
+    last_orientation.setW(odom->pose.pose.orientation.w);
 }
+// Constructor y Destructor
+RobotBaseDriver::RobotBaseDriver(){
+	// ros::init("RobotBaseDriver_node");
+	ROS_DEBUG("RobotBaseDriver: Creando nodeHandle");
+	nh_ = new ros::NodeHandle;
+	// Subscribir a mensajes de odometría
+	ROS_DEBUG("RobotBaseDriver: Creando subscriber odom");
+	odom_sub_ = nh_->subscribe<nav_msgs::Odometry>(Util::ODOM_TOPIC, 1, odomCallback);
+	// Publicar mensajes para base cmd
+	ROS_DEBUG("RobotBaseDriver: Creando publisher twist");
+	base_cmd_pub_ = nh_->advertise<geometry_msgs::Twist>(Util::BASE_CONTROLLER_TOPIC, 1);
+}
+RobotBaseDriver::~RobotBaseDriver(){
+	ROS_DEBUG("RobotBaseDriver: Destruyendo nodeHandle");
+	delete nh_;
+}
+/*bool RobotBaseDriver::goToPose(const string frame_id, geometry_msgs::PoseStamped pose){
+	// Ver si es el mismo frame, sino, transformarlo
+}*/
+bool RobotBaseDriver::goToPose(){
+	travel(-1, 0);
+	turn(Util::toRad(-90));
+}
+// PRIVATE
+bool RobotBaseDriver::turn(float angle){
+	ROS_DEBUG("Girar %f grados", Util::toGrad(angle));
+	ros::Rate rate(LOOP_FREQ);
+	store_odom = true;
+	rate.sleep();
+	// Actualizar última posición
+	ros::spinOnce();
+	tf::Quaternion start_orientation = last_orientation;
+	geometry_msgs::Twist base_cmd;
+	base_cmd.angular.z = (angle < 0 ? -1 : 1)*ANGULAR_VELOCITY;
+	bool done = false;
+	float real_angle = 0;
+	float traveled_angle;
+	while (!done and ros::ok()){
+		base_cmd_pub_.publish(base_cmd);
+		rate.sleep();
+		// Actualizar posicíón odométrica
+		ros::spinOnce();
+		traveled_angle = 2*start_orientation.angle(last_orientation);
+		if (traveled_angle > (angle < 0 ? -angle : angle)) done = true;
+	}
+	if (done){
+		ROS_DEBUG("Giro real fue: %f grados", Util::toGrad(traveled_angle));
+		return true;
+	}
+	else{
+		ROS_ERROR("RobotBaseDriver: What wea dis error, i am fallan2");
+		return false;
+	}
+}
+bool RobotBaseDriver::travel(float distance, float angle){
+	ROS_DEBUG("RobotBaseDriver: Desplazar distancia: %f, angulo: %f", distance, angle);
+	ros::Rate rate(LOOP_FREQ);
+	store_odom = true;
+	rate.sleep();
+	ros::spinOnce();
+	tf::Vector3 start_position = last_position;
+	// mensaje a enviar a la base
+	geometry_msgs::Twist base_cmd;
+	// Calcular comando a enviar
+	tf::Vector3 itongo((distance < 0 ? -1 : 1)*TWIST_VELOCITY, 0, 0);
+	tf::Vector3 zaxis(0,0,1);
+	// Rotar vector unitario
+	itongo = itongo.rotate(zaxis, angle);
+	// Guardar en twist
+	base_cmd.linear.x = itongo.x();
+	base_cmd.linear.y = itongo.y();
+	base_cmd.linear.z = 0; // No queremos flotar ni enterrarnos en el suelo
+	bool done = false;
+	float real_distance = 0;
+	float traveled_dist;
+	while (!done and ros::ok()){
+		// Enviar comando (ponderado)
+		base_cmd_pub_.publish(base_cmd);
+		rate.sleep();
+		// Actualizar posición odométrica
+		ros::spinOnce();
+		traveled_dist = start_position.distance(last_position);
+		ROS_DEBUG("RobotBaseDriver: Distancia navegada = %fm", traveled_dist);
+		if (traveled_dist > (distance < 0 ? -distance : distance)) done = true;
+	}
+	store_odom = false;
+	if (done){
+		ROS_DEBUG("RobotBaseDriver: Desplazamiento real fue: %fm", traveled_dist);
+		return true;
+	}
+	else{
+		ROS_ERROR("RobotBaseDriver: What wea dis error, i am fallan2.");
+		return false;
+	}
+}
+
+/**
+ *	TODO:
+ *		- Evaluar posibilidad de desacelerar al acercarse a posición final (para turn y travel).
+ * 
+ */
